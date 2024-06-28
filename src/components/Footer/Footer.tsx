6943bb95b6b50f06666e67f712c9025ec0ef8818
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2024-06-28 16:51:00 -0700

    Changed footer icons, dimmed glow

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 // import Image from 'next/image';
-import { FaInstagram, FaYoutube, FaTwitter, FaGithub, FaGitlab } from 'react-icons/fa';
+import { FaInstagram, FaYoutube, FaTwitter, FaGithub, FaGitlab, FaTwitch, FaMailBulk, FaEnvelope } from 'react-icons/fa';
 import styles from './Footer.module.css';
 // import logo from '../../../public/images/Logo.svg';
 
@@ -22,7 +22,7 @@ export default function Footer() {
                     </div>
                     <small className={styles['website-rights']}>CUZETH &copy; {new Date().getFullYear()}</small>
                     <div className={styles['social-icons']}>
-                        <a
+                        {/* <a
                             className={styles['social-icon-link']}
                             href="https://www.instagram.com/cuzeth/"
                             target="_blank"
@@ -30,7 +30,7 @@ export default function Footer() {
                             aria-label="Instagram"
                         >
                             <FaInstagram />
-                        </a>
+                        </a> */}
                         <a
                             className={styles['social-icon-link']}
                             href="https://www.youtube.com/channel/UCYVD0wcSlHFlCs41kHTap2Q/"
@@ -40,7 +40,7 @@ export default function Footer() {
                         >
                             <FaYoutube />
                         </a>
-                        <a
+                        {/* <a
                             className={styles['social-icon-link']}
                             href="https://twitter.com/Cuzeth"
                             target="_blank"
@@ -48,6 +48,24 @@ export default function Footer() {
                             aria-label="Twitter"
                         >
                             <FaTwitter />
+                        </a> */}
+                        <a
+                            className={styles['social-icon-link']}
+                            href="https://twitch.tv/TheRealCuzeth"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="Twitch"
+                        >
+                            <FaTwitch />
+                        </a>
+                        <a
+                            className={styles['social-icon-link']}
+                            href="mailto:cuz@cuzeth.com"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="E-Mail"
+                        >
+                            <FaEnvelope />
                         </a>
                         <a
                             className={styles['social-icon-link']}
